f780e4fd4eb3e0ff3545dbe45a60858a274b8668
うい 椎名 <shiina04jbs10@outlook.com>
2026-05-15 08:53:57 +0900

    開始

---
+++ Logger/Logger.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+//@brief	| 自作ログ名前空間 |
+namespace Logger {
+
+	//@brief	| ログレベル |
+	enum class log_level {
+		info,warrning,error
+	};
+	
+	//@brief	| ログの種類 |
+	enum class log_type {
+
+	};
+
+};
